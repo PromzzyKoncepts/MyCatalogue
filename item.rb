b898cfb549c8951d4b06b63381b017f0335cefd3
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-02-14 11:56:46 +0100

    Add methods for the Item class

--- item.rb
+++ item.rb
@@ -1,9 +1,28 @@
 class Item
   attr_accessor :genre, :author, :label, :archived, :publish_date
 
-  def initialize(id, publish_date, archived)
-  @id = Random.rand(1...1000)
-  @publish_date = publish_date
-  @archived = archived
+  def initialize(id, publish_date, archived: false)
+    @id = Random.rand(1...1000)
+    @publish_date = publish_date
+    @archived = archived
   end
+
+  def move_to_achive
+    if Item.can_be_archived? 
+      @archived = true
+    else
+      return
+    end
+  end
+
+  private
+  def can_be_archived?
+    current_year = Time.new.year 
+    if (current_year - @publish_date) > 10
+      true
+    else
+      false
+    end
+  end
+
 end
