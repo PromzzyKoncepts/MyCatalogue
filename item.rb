b8da760834edce2d47019db7e901be34f7e36466
Suzi216 <suzana.marsela@gmail.com>
2023-02-14 14:15:17 +0300

    Adding the main.rb file and fixed the linters

--- item.rb
+++ item.rb
@@ -1,28 +1,22 @@
 class Item
   attr_accessor :genre, :author, :label, :archived, :publish_date
 
-  def initialize(id, publish_date, archived: false)
+  def initialize(_id, publish_date, archived: false)
     @id = Random.rand(1...1000)
     @publish_date = publish_date
     @archived = archived
   end
 
   def move_to_achive
-    if Item.can_be_archived? 
-      @archived = true
-    else
-      return
-    end
+    return unless Item.can_be_archived?
+
+    @archived = true
   end
 
   private
-  def can_be_archived?
-    current_year = Time.new.year 
-    if (current_year - @publish_date.to_i) > 10
-      true
-    else
-      false
-    end
-  end
 
+  def can_be_archived?
+    current_year = Time.new.year
+    (current_year - @publish_date.to_i) > 10
+  end
 end
